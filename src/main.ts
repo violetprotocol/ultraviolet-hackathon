4ee0bc20a66836462cfdf4f0a3aa6637034a2979
Valentin Manes <valentinmanes@outlook.fr>
2022-02-15 22:47:52 +0100

    Add front

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 const PORT = process.env.port || 8080;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: true, credentials: true });
   await app.listen(PORT);
 }
 bootstrap();
