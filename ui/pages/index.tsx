7c8ae6e172a3e36af8144e679c40287a96ed62c3
Valentin Manes <valentinmanes@outlook.fr>
2022-02-15 23:07:06 +0100

    Triggers CORS issue

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -1,9 +1,25 @@
 import type { NextPage } from "next";
+import { useState, useEffect } from "react";
+
+const backUrl = "http://localhost:3001";
 
 const Home: NextPage = () => {
+  const [hello, setHello] = useState<string>("nothing");
+
+  useEffect(() => {
+    console.log("fetching...", backUrl);
+    fetch(backUrl, { credentials: "include" })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("back data:", data);
+        setHello(data);
+      });
+  }, []);
+
   return (
     <>
       <h1>Home</h1>
+      <p>Hello from back? {hello}</p>
     </>
   );
 };
