742704ce735834a68803a99642c18ef48898b327
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-02-17 13:18:17 -0700

    remove previous web3 folder

--- web3/scripts/1_deploy_token.py
+++
@@ -1,9 +0,0 @@
-from brownie import accounts, config, TokenERC20
-
-initialSupply = 100000000000000000000
-tokenName = "TOKEN"
-tokenSymbol = "TKN"
-
-def main():
-    account = accounts[0]
-    erc20 = TokenERC20.deploy(initialSupply, tokenName, tokenSymbol, {"from": account})
