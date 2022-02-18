19bcf0977e617b91fa1d881b23b15b0384b35cf5
Papa Smurf <chris@violet.co>
2022-02-18 15:22:18 -0700

    cleaned loans page

--- ui/pages/dashboard.tsx
+++ ui/pages/dashboard.tsx
@@ -14,25 +14,6 @@ import LitJsSdk from "lit-js-sdk";
 
 const Dashboard: NextPage = () => {
   const { loan, setLoan } = useContext(LoanContext);
-  const { passport, setPassport } = useContext(PassportContext);
-
-  const [file, setFile] = useState();
-  const [
-    { data: networkData, error: networkError, loading: networkLoading },
-    switchNetwork,
-  ] = useNetwork();
-  const [{ data, error, loading }, getSigner] = useSigner();
-  const contract = useContract({
-    addressOrName: contracts.nft,
-    contractInterface: nftABI,
-    signerOrProvider: data,
-  });
-
-  const [sending, setSending] = useState(false);
-
-  function handleFile(event) {
-    setFile(event.target.files[0]);
-  }
 
   useEffect(() => {
     if (!loan.borrowerAddr) {
@@ -41,77 +22,6 @@ const Dashboard: NextPage = () => {
       Router.push("/");
     }
   }, [loan]);
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormData>();
-
-  const onMint = async () => {
-    setSending(true);
-    const address = await (await getSigner()).getAddress();
-    const index = await contract.callStatic.totalSupply();
-    const res = await contract.mint(index, address);
-    await res.wait();
-
-    await submitPassport(address, index);
-
-    setSending(false);
-  };
-
-  const submitPassport = async (owner: string, id: number) => {
-    const client = new LitJsSdk.LitNodeClient();
-    await client.connect();
-
-    const { symmetricKey, encryptedZip } = await LitJsSdk.zipAndEncryptFiles([
-      file,
-    ]);
-
-    const accessControlConditions = [
-      {
-        contractAddress: contracts.nft,
-        standardContractType: "ERC721",
-        chain: networkData.chain?.name.toLowerCase(),
-        method: "ownerOf",
-        parameters: [id.toString()],
-        returnValueTest: {
-          comparator: "==",
-          value: owner,
-        },
-      },
-    ];
-
-    const authSig = await LitJsSdk.checkAndSignAuthMessage({
-      chain: networkData.chain?.name.toLowerCase(),
-    });
-
-    const encryptedSymmetricKey = await window.litNodeClient.saveEncryptionKey({
-      accessControlConditions,
-      symmetricKey,
-      authSig,
-      chain: networkData.chain?.name.toLowerCase(),
-    });
-
-    const encryptedZipB64 = Buffer.from(
-      await encryptedZip.arrayBuffer(),
-    ).toString("base64");
-
-    fetch("http://localhost:8080/api/store", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      credentials: "include" as RequestCredentials,
-      body: JSON.stringify({
-        nftId: id.toString(),
-        encryptedFile: encryptedZipB64,
-        encryptedSymmetricKey: encryptedSymmetricKey.toString(),
-        accessControlConditions: accessControlConditions,
-      }),
-    }).then(async (response) => {
-      console.log(response.text());
-      Router.push("/borrow");
-    });
-  };
 
   const onNewLoan = () => {
     Router.push("/escrow");
