0ed06a5d21c64e79a7baf61d78adfe539079b5f1
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-02-18 16:04:27 -0700

    update abi and addresses from deployment

--- ui/constants/contracts.ts
+++ ui/constants/contracts.ts
@@ -1,5 +1,5 @@
 const contracts = {
-  lendingPool: "",
+  lendingPool: "0x03Fa1ff6d4269C045a0B8Bc36aB96F22b94BC386",
   nft: "0x894248F5422DC6f93CB61820A390dC99cD288AD5",
   dai: "0xff795577d9ac8bd7d90ee22b6c1703490b6512fd",
 };
