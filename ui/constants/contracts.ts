be2c1a339679629c3baefaa4c6114be41b37df6f
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-02-18 15:27:52 -0700

    add kovan uv nft address

--- ui/constants/contracts.ts
+++ ui/constants/contracts.ts
@@ -1,7 +1,7 @@
 const contracts = {
-    lendingPool: "",
-    nft: "0xb5825059842313F98e82e54Da0186d9771438ab3",
-    dai: "0xff795577d9ac8bd7d90ee22b6c1703490b6512fd"
-}
+  lendingPool: "",
+  nft: "0x894248F5422DC6f93CB61820A390dC99cD288AD5",
+  dai: "0xff795577d9ac8bd7d90ee22b6c1703490b6512fd",
+};
 
-export default contracts
+export default contracts;
