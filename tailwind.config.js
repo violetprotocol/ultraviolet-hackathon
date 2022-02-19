4c4eaea471a09d35788f79639a88fa154cc5249f
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-02-19 16:11:34 -0700

    add custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+
 module.exports = {
   content: ['./ui/pages/**/*.{js,ts,jsx,tsx}', './ui/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'teletactile': ['teletactile', 'ui-monospace', 'SFMono-Regular', 'Menlo'],
+      }
+    },
   },
   plugins: [],
 }
