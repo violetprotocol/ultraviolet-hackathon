db7d04925be27119f36dfd7feaeb2b61c1da9270
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-02-20 00:10:48 -0700

    remove shadow

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -4,10 +4,16 @@ import Image from "next/image";
 import Router from "next/router";
 import { useContext, useEffect, useState } from "react";
 import { SiweMessage } from "siwe";
-import { useAccount, useConnect, useContract, useSigner, useSignMessage } from "wagmi";
+import {
+  useAccount,
+  useConnect,
+  useContract,
+  useSigner,
+  useSignMessage,
+} from "wagmi";
 import contracts from "../constants/contracts";
-import { LoanContext } from "../lib/context";
 import lendingPoolABI from "../constants/lendingpool.json";
+import { LoanContext } from "../lib/context";
 
 const Home: NextPage = () => {
   const [{ data, error }, connect] = useConnect();
@@ -57,14 +63,14 @@ const Home: NextPage = () => {
       const connectedAddress = await signer.getAddress();
       const owner = await contract.owner();
       console.log(owner);
-  
+
       if (connectedAddress == owner) {
         Router.push("/lender");
       } else {
         Router.push("/dashboard");
       }
     }
-  }
+  };
 
   useEffect(() => {
     if (signature && siweMessage) {
@@ -92,7 +98,7 @@ const Home: NextPage = () => {
       <div className="container mx-auto px-4 h-full">
         <div className="flex content-center items-center justify-center h-full w-full">
           <div className="w-full lg:w-6/12 px-4">
-            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
+            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 rounded-lg bg-blueGray-200 border-0">
               <div className="rounded-t mb-0 px-6 py-6">
                 <div className="text-center mb-3">
                   <h2 className="title font-teletactile">
