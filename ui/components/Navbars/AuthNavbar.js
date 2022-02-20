b5e47328c77ea72a1b341d5923a0c8ed672f9ae4
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-02-20 16:17:17 -0700

    cleaning

--- ui/components/Navbars/AuthNavbar.js
+++ ui/components/Navbars/AuthNavbar.js
@@ -1,10 +1,9 @@
-import React from "react";
 import Link from "next/link";
+import React from "react";
 // components
-
 import PagesDropdown from "../../components/Dropdowns/PagesDropdown.js";
 
-export default function Navbar(props) {
+export default function Navbar() {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     <>
