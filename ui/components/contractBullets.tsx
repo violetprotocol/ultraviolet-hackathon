7b7e746c8963ce8ea30751f60d783fa26515323f
Gabriel Ferraz <sudoferraz@gmail.com>
2022-02-20 02:19:21 -0700

    Trying to fix contract

--- ui/components/contractBullets.tsx
+++ ui/components/contractBullets.tsx
@@ -3,9 +3,16 @@ import { computeDue } from "../lib/computeDue";
 const Point = ({ name, body }) => {
   return (
     <li>
-      <span style={{ fontSize: "20px", paddingRight: "10px" }}>
-        <i className="neon-red">{name}</i>
-      </span>: {body}
+      <span
+        style={{
+          fontSize: "20px",
+          paddingRight: "10px",
+          fontFamily: "ui-monospace",
+        }}
+      >
+        <i className="text-black">{name}</i>
+      </span>
+      : {body}
     </li>
   );
 };
@@ -20,11 +27,18 @@ const ColorText = ({ text }) => {
 
 const ContractBullets = ({ loan }) => {
   return (
-    <ol>
+    <ol className="bg-white rounded p-4">
       <Point
         name="Parties"
         body={
-          <span className="title font-teletactile" style={{width:"100%", fontSize: "13px"}}>
+          <span
+            className="title text-black"
+            style={{
+              width: "100%",
+              fontSize: "13px",
+              fontFamily: "SFMono-Regular",
+            }}
+          >
             The personal loan agreement made <Today /> between{" "}
             <ColorText text={loan.lenderAddr} /> ("Party A") and{" "}
             <ColorText text={loan.borrowerAddr} /> ("Party B").
@@ -36,63 +50,102 @@ const ContractBullets = ({ loan }) => {
         }
       />
 
-      <br/>
       <Point
         name="Loan"
         body={
-          <span className="title font-teletactile" style={{width:"100%", fontSize: "13px"}}>
+          <span
+            className="title text-back"
+            style={{
+              width: "100%",
+              color: "black",
+              fontSize: "13px",
+              fontFamily: "SFMono-Regular",
+            }}
+          >
             Party A lends <ColorText text={`${loan.amount} DAI`} /> to Party B.
           </span>
         }
       />
 
-      <br/>
       <Point
         name="Interest"
         body={
-          <span className="title font-teletactile" style={{width:"100%", fontSize: "13px"}}>
+          <span
+            className="title text-black"
+            style={{
+              width: "100%",
+              fontSize: "13px",
+              fontFamily: "ui-monospace",
+            }}
+          >
             Party B shall pay <ColorText text="10% APY" /> in interest.
           </span>
         }
       />
 
-      <br/>
+      <br />
       <Point
         name="Payment"
         body={
-          <span className="title font-teletactile" style={{width:"100%", fontSize: "13px"}}>
+          <span
+            className="title text-black"
+            style={{
+              width: "100%",
+              fontSize: "13px",
+              fontFamily: "SFMono-Regular",
+            }}
+          >
             Party B shall pay{" "}
             <ColorText text={`${computeDue(loan).toFixed(3)} DAI`} /> to Party A
-            before the loan ends on <ColorText text={new Date(loan.maturity).toLocaleString()} />.
+            before the loan ends on{" "}
+            <ColorText text={new Date(loan.maturity).toLocaleString()} />.
           </span>
         }
       />
 
-      <br/>
       <Point
         name="Payment Instruction"
         body={
-          <span className="title font-teletactile" style={{width:"100%", fontSize: "13px"}}>
+          <span
+            className="title text-black"
+            style={{
+              width: "100%",
+              fontSize: "13px",
+              fontFamily: "SFMono-Regular",
+            }}
+          >
             Payment must be made to <ColorText text={loan.lenderAddr} />.
           </span>
         }
       />
 
-      <br/>
       <Point
         name="Prepayment"
         body={
-          <span className="title font-teletactile" style={{width:"100%", fontSize: "13px"}}>
+          <span
+            className="title text-black"
+            style={{
+              width: "100%",
+              fontSize: "13px",
+              fontFamily: "SFMono-Regular",
+            }}
+          >
             Party B shall be entitled to repay the loan in full at any time
           </span>
         }
       />
 
-      <br/>
       <Point
         name="Identification"
         body={
-          <span className="title font-teletactile" style={{width:"100%", fontSize: "13px"}}>
+          <span
+            className="title text-black"
+            style={{
+              width: "100%",
+              fontSize: "13px",
+              fontFamily: "SFMono-Regular",
+            }}
+          >
             Party B shall deposit his/her <ColorText text="Passport" /> in
             Identity Escrow. In case of default, Party A will be given access to
             the <ColorText text="Passport" />.
@@ -100,15 +153,21 @@ const ContractBullets = ({ loan }) => {
         }
       />
 
-      <br/>
       <Point
         name="Identification"
         body={
-          <span className="title font-teletactile" style={{width:"100%", fontSize: "13px"}}>
+          <span
+            className="title text-black"
+            style={{
+              width: "100%",
+              fontSize: "13px",
+              fontFamily: "SFMono-Regular",
+            }}
+          >
             The Parties agree to follow UltraViolet's{" "}
             <a href="https://violet.co">Privacy Policy</a>.
-            <br/>
-            <br/>
+            <br />
+            <br />
             <p>
               IN WITNESS WHEREOF, the Parties have executes this Agreement as of
               the undersigned dates written below
