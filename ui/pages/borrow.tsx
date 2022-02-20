86d0eec1e10e0acbe94db023b07a66a53b2cfb2f
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-02-20 01:03:06 -0700

    small ui changes

--- ui/pages/borrow.tsx
+++ ui/pages/borrow.tsx
@@ -1,12 +1,11 @@
+import { BigNumber } from "ethers";
 import type { NextPage } from "next";
 import Router from "next/router";
-import { useEffect, useContext } from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
-
-import FormData from "../lib/formData";
+import { useContext, useEffect } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import FormInput from "../components/formInput";
 import { LoanContext } from "../lib/context";
-import { BigNumber } from "ethers";
+import FormData from "../lib/formData";
 
 const Borrow: NextPage = () => {
   const { loan, setLoan } = useContext(LoanContext);
@@ -26,8 +25,8 @@ const Borrow: NextPage = () => {
 
   const onSubmit: SubmitHandler<FormData> = (data) => {
     loan.amount = BigNumber.from(data.amount).mul(BigNumber.from("10").pow(18));
-    console.log(Date.parse(data.maturity)/1000);
-    loan.maturity = Date.parse(data.maturity)/1000;
+    console.log(Date.parse(data.maturity) / 1000);
+    loan.maturity = Date.parse(data.maturity) / 1000;
     setLoan(loan);
     Router.push("/contract");
   };
@@ -37,10 +36,13 @@ const Borrow: NextPage = () => {
       <h1 style={{ fontSize: "30px" }}>
         <i className="neon-green">Set your borrowing terms</i>
       </h1>
-      <br/>
-      <br/>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <h2 className="title font-teletactile" style={{ fontSize: "13px" }}>
+      <br />
+      <br />
+      <form onSubmit={handleSubmit(onSubmit)} className="mt-8">
+        <h2
+          className="title font-teletactile text-left"
+          style={{ fontSize: "13px" }}
+        >
           1. How much do you want to borrow?
         </h2>
         <FormInput
@@ -51,8 +53,11 @@ const Borrow: NextPage = () => {
           placeholder={0}
         />
 
-        <br/>
-        <h2 className="title font-teletactile" style={{ fontSize: "13px" }}>
+        <br />
+        <h2
+          className="title font-teletactile text-left"
+          style={{ fontSize: "13px" }}
+        >
           2. Select the maturity of your loan.
         </h2>
         <FormInput
